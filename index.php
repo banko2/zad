b153f6990f24d48de9fdacf73b2c19006a556a17
banko2 <60343173+banko2@users.noreply.github.com>
2020-02-07 06:27:46 +0200

    add button

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href='style.css'>
+
+</head>
+<body>
+
 <style>
+
 div.gallery {
-  margin: 5px;
-  border: 1px solid #ccc;
+  margin: 10px;
+  border: 2px solid #ccc;
   float: left;
   width: 180px;
 }
 
 div.gallery:hover {
-  border: 1px solid #777;
+  border: 3px solid #777;
 }
 
 div.gallery img {
@@ -18,40 +29,43 @@ div.gallery img {
 }
 
 div.desc {
-  padding: 15px;
+  border: 2px solid black;
+  padding-bottom: 10%;
+  background-color:yellow; color:red;font-size: 150%;
   text-align: center;
 }
 </style>
 </head>
-<body>
+<header>Woman galery</header>
+
+<body style="background-color:lightblue;">
 
 <div class="gallery">
-  <a target="_blank" href="ital1.jpg">
+  <a target="_self" href="ital3.mp4">
     <img src="ital2.jpg" width="800" height="600">
   </a>
-  <div class="desc">Woman</div>
+  <div class="desc">Italian</div>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="image1.jpg">
+  <a target="_self" href="image1.mp4">
     <img src="ital1.jpg"  width="800" height="600">
   </a>
-  <div class="desc">Woman1</div>
+  <div class="desc">French</div>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="bulg.mp4">
+  <a target="_self" href="bulg.mp4">
     <img src="bulg.jpg"  width="800" height="600">
   </a>
-  <div class="desc">Woman2</div>
+  <div class="desc">Bulgarian</div>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="image3.jpg">
+  <a target="_self" href="image3.mp4">
     <img src="usa.jpg"  width="800" height="600">
   </a>
-  <div class="desc">Woman3</div>
+  <div class="desc">USA</div>
 </div>
-
 </body>
 </html>
